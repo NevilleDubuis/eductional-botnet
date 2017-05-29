ae1dae26e4e561d7cc67fa4069fd7d2e9e694644
Thierry Auberson <tchery.auberson@gmail.com>
2017-05-29 20:55:09 +0200

    add admin status to users table and seeder

---
+++ src/web/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+          'name' => 'Botnet Admin',
+          'email' => 'educational.botnet@gmail.com',
+          'password' => bcrypt('adminbotnet'),
+          'admin' => true,
+          'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
