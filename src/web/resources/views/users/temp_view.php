73e4ecde583e66582c30622172704acf584b84a1
Thierry Auberson <tchery.auberson@gmail.com>
2017-06-12 20:31:45 +0200

    add files

---
+++ src/web/resources/views/users/temp_view.php
@@ -0,0 +1,6 @@
+<td>{!! link_to_route('user.show', 'Voir', [$user->id], ['class' => 'btn btn-success btn-block']) !!}</td>
+
+
+
+{!! link_to_route('user.create', 'Ajouter un utilisateur', [], ['class' => 'btn btn-info pull-right']) !!}
+{!! $links !!}
