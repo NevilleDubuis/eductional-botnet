0e85f3f826a634cc831f218663a990161655a93e
Laurent Borgognon <laurent@bggn.ch>
2017-06-14 18:33:33 +0200

    Add groups table

---
+++ src/web/database/migrations/2017_06_14_161511_create_users_groups_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('group_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->foreign('group_id')->references('id')->on('groups')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users_groups', function (Blueprint $table) {
+            $table->dropForeign('users_groups_user_id_foreign');
+            $table->dropForeign('users_groups_group_id_foreign');
+        });
+        Schema::dropIfExists('users_groups');
+    }
+}
