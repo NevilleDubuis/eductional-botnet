2292db2a406d8ac1293705c1666cb3a99e3200cb
Néville Dubuis <ndubuis@gmail.com>
2017-06-19 21:41:31 +0200

    enhance bot model

--- src/web/app/Bot.php
+++ src/web/app/Bot.php
@@ -8,19 +8,63 @@ use App\Attack;
 class Bot extends Model
 {
   /**
-    * The attributes that are mass assignable.
-    *
-    * @var array
-    */
-   protected $fillable = ['mac_address', 'name', 'cpu', 'operating_system', 'state'];
+  * The attributes that are mass assignable.
+  *
+  * @var array
+  */
+  protected $fillable = ['mac_address', 'name', 'cpu', 'operating_system', 'state'];
 
-   public function needInformation()
-   {
-      return (!$this->name);
-   }
+  /**
+  * Scope a query to get only connected bots.
+  *
+  * @param \Illuminate\Database\Eloquent\Builder $query
+  * @return \Illuminate\Database\Eloquent\Builder
+  */
+  public function scopeConnected($query)
+  {
+    return $query->where('state', '!=', 'disconnected');
+  }
 
-   public function attack()
-   {
-     return $this->belongsTo('App\Attack', 'attack_id');
-   }
+  /**
+  * Scope a query to get only disconnected bots.
+  *
+  * @param \Illuminate\Database\Eloquent\Builder $query
+  * @return \Illuminate\Database\Eloquent\Builder
+  */
+  public function scopeDisconnected($query)
+  {
+    return $query->ofState('disconnected');
+  }
+
+  /**
+  * Scope a query to only include users of a given type.
+  *
+  * @param \Illuminate\Database\Eloquent\Builder $query
+  * @param mixed $type
+  * @return \Illuminate\Database\Eloquent\Builder
+  */
+  public function scopeOfState($query, $state)
+  {
+    return $query->where('state', $state);
+  }
+
+  /**
+  * true if we need iformation form bot.
+  *
+  * @return Boolean
+  */
+  public function needInformation()
+  {
+    return (!$this->name);
+  }
+
+  /**
+  * Get the attack that owns the bot.
+  *
+  * @return App\Attack;
+  */
+  public function attack()
+  {
+    return $this->belongsTo('App\Attack', 'attack_id');
+  }
 }
