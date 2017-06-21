64cc9fae3d47a57d791733f581aed4c546bfbfbe
Néville Dubuis <ndubuis@gmail.com>
2017-06-21 18:48:00 +0200

    add scope to attacks

--- src/web/app/Attack.php
+++ src/web/app/Attack.php
@@ -8,12 +8,45 @@ use App\Bot;
 class Attack extends Model
 {
   /**
-    * The attributes that are mass assignable.
-    *
-    * @var array
-    */
-   protected $fillable = ['name', 'target', 'port', 'start', 'finish', 'bots_number', 'duration'];
+  * The attributes that are mass assignable.
+  *
+  * @var array
+  */
+  protected $fillable = ['name', 'target', 'port', 'start', 'finish', 'bots_number', 'duration'];
 
+
+  /**
+  * Scope a query to get only currently connected bots.
+  *
+  * @param \Illuminate\Database\Eloquent\Builder $query
+  * @return \Illuminate\Database\Eloquent\Builder
+  */
+  public function scopeWaiting($query)
+  {
+    return $query->whereNull('start');
+  }
+
+  /**
+  * Scope a query to get only currently connected bots.
+  *
+  * @param \Illuminate\Database\Eloquent\Builder $query
+  * @return \Illuminate\Database\Eloquent\Builder
+  */
+  public function scopeRunning($query)
+  {
+    return $query->whereNotNull('start')->whereNull('end');
+  }
+
+  /**
+  * Scope a query to get only currently connected bots.
+  *
+  * @param \Illuminate\Database\Eloquent\Builder $query
+  * @return \Illuminate\Database\Eloquent\Builder
+  */
+  public function scopeFinnished($query)
+  {
+    return $query->whereNotNull('start')->whereNotNull('end');
+  }
 
   public function bots()
   {
